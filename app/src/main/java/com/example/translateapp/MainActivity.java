6054725d7db1d7076f3237e0954e69866c173772
E-Ramos94 <enrique.ramos@laboratoriosolaris.com>
2023-06-07 10:37:34 -0600

    Integración de función de descarga de paquetes de traduccion

--- app/src/main/java/com/example/translateapp/MainActivity.java
+++ app/src/main/java/com/example/translateapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.translateapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,8 +15,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.translateapp.Modelo.Idioma;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     EditText Et_Idioma_Origen;
     TextView Tv_Idioma_Destino;
     MaterialButton Btn_Elegir_Idioma, Btn_Idioma_Elegido, Btn_Traducir;
+    private ProgressDialog progressDialog;
 
     private ArrayList<Idioma> IdiomasrrayList;
 
@@ -34,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     private String titulo_idioma_origien = "Español";
     private String codigo_idioma_destino = "en";
     private String titulo_idioma_destino = "Ingles";
+
+    private TranslatorOptions translatorOptions;
+    private Translator translator;
+    private String texto_idioma_origen = "";
 
 
     @Override
@@ -62,7 +75,8 @@ public class MainActivity extends AppCompatActivity {
         Btn_Traducir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Traducir", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "Traducir", Toast.LENGTH_SHORT).show();
+                ValidarDatos();
             }
         });
     }
@@ -73,6 +87,11 @@ public class MainActivity extends AppCompatActivity {
         Btn_Elegir_Idioma = findViewById(R.id.Btn_Elegir_Idioma);
         Btn_Idioma_Elegido = findViewById(R.id.Btn_Idioma_Elegido);
         Btn_Traducir = findViewById(R.id.Btn_Traducir);
+
+        progressDialog = new ProgressDialog(this);
+
+        progressDialog.setTitle("Espere por favor...");
+        progressDialog.setCanceledOnTouchOutside(false);
     }
 
     private void IdiomasDisponibles() {
@@ -143,5 +162,67 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void ValidarDatos() {
+        texto_idioma_origen = Et_Idioma_Origen.getText().toString().trim();
+        Log.d(REGISTROS, "ValidarDatos: texto_idioma_origen: " + texto_idioma_origen);
+
+        if (texto_idioma_origen.isEmpty()){
+            Toast.makeText(MainActivity.this, "Ingrese texto", Toast.LENGTH_SHORT).show();
+        } else {
+            TraducirTexto();
+        }
+    }
+
+    private void TraducirTexto() {
+        progressDialog.setMessage("Porcesando...");
+        progressDialog.show();
+
+        translatorOptions = new TranslatorOptions.Builder()
+                .setSourceLanguage(codigo_idioma_origen)
+                .setTargetLanguage(codigo_idioma_destino)
+                .build();
+
+        translator = Translation.getClient(translatorOptions);
+
+        DownloadConditions downloadConditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+
+        translator.downloadModelIfNeeded(downloadConditions)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //Los paquetes de traduccion se descargaron con exito
+                        Log.d(REGISTROS, "onSuccess: El paquete se ha descargado con exito");
+                        progressDialog.setMessage("Traduciendo texto...");
+
+                        translator.translate(texto_idioma_origen)
+                                .addOnSuccessListener(new OnSuccessListener<String>() {
+                                    @Override
+                                    public void onSuccess(String texto_traducido) {
+                                        progressDialog.dismiss();
+                                        Log.d(REGISTROS, "onSuccess: texto_traducido: " + texto_traducido);
+                                        Tv_Idioma_Destino.setText(texto_traducido);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        progressDialog.dismiss();
+                                        Log.d(REGISTROS, "onFailure "+e);
+                                        Toast.makeText(MainActivity.this, "" +e, Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Los paquetes no se descargaron
+                        progressDialog.dismiss();
+                        Log.d(REGISTROS, "onFailure "+e);
+                        Toast.makeText(MainActivity.this, "" +e, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
